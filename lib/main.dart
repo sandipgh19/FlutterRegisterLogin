7d3031c7dcba05bf0449e39545d398884dc6fb6d
sandip <sandipgh19@gmail.com>
2020-12-21 21:04:57 +0530

    refactor: ui for login page

--- lib/main.dart
+++ lib/main.dart
@@ -50,18 +50,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -72,11 +61,6 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
@@ -97,21 +81,82 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            new Container(
+              child: Text("User Login",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.orange,
+              ),
+              )
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            new Container(
+              margin: const EdgeInsets.all(20),
+              child: TextField(
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                border: new OutlineInputBorder(
+                  borderRadius: const BorderRadius.all(
+                    const Radius.circular(18.0)
+                  )
+                ),
+                filled: true,
+                hintText: "User name"
+              ),
             ),
+            ),
+            new Container(
+              margin: const EdgeInsets.all(20),
+              child: TextField(
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                border: new OutlineInputBorder(
+                  borderRadius: const BorderRadius.all(
+                    const Radius.circular(18.0)
+                  )
+                ),
+                filled: true,
+                hintText: "Password"
+              ),
+            ),
+            ),
+            new Container(
+              margin: const EdgeInsets.all(20),
+              child: new MaterialButton(
+                minWidth: double.infinity,
+                height: 44,
+                shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(30.0)
+                ),
+                color: Colors.orange,
+                onPressed: () {},
+                child: Text('Login',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16
+                  ),
+                )
+              ),
+            ),
+            new Container(
+              child: Text("Don't have account Click here",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.orange
+              ),
+              )
+            ),
+            new Container(
+              margin: const EdgeInsets.all(20),
+              child: Text("Forgot Password?",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black
+              ),
+              )
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
     );
   }
 }
